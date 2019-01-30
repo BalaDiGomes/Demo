aa65b9519f34c8c2f27baa29080f017e0f416ee3
Marcelo Szalontai <marcelo.szalontai@linx.com.br>
2019-01-30 00:11:04 -0200

    Primeiro update

---
+++ App1/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App1.Service.Model;
+using App1.Service;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Button.Clicked += SearchCEP;
+
+        }
+        private void SearchCEP(object sender,EventArgs args)
+        {
+            string cep = Cep.Text.Trim();
+            Address add = ViaCEPService.SearchAddressViaCEP(cep);
+            Result.Text = string.Format("Endereço: {2} de {3} {0},{1} ", add.localidade,add.uf,add.logradouro,add.bairro);
+             
+        }
+    }
+}
---
+++ App1/App1/App1/Service/ViaCEPService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App1.Service.Model;
+using Newtonsoft.Json;
+
+namespace App1.Service
+{
+    public class ViaCEPService
+    {
+        private static string AddressURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Address SearchAddressViaCEP(string cep)
+        {
+            string newAddressURL = string.Format(AddressURL, cep);
+            WebClient wc = new WebClient();
+            string Content = wc.DownloadString(newAddressURL);
+            Address Add = JsonConvert.DeserializeObject<Address>(Content);
+
+            return Add;
+        }
+    }
+}
